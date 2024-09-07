ccfe13d897ffc6f4934de11e26cfba1e5041335d
admin <suhasy983@gmail.com>
2024-09-08 00:20:25 +0530

    updated runner

--- infra-create/main.tf
+++ infra-create/main.tf
@@ -3,18 +3,20 @@ resource "aws_instance" "tool" {
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.tool-sg.id]
   iam_instance_profile   = aws_iam_instance_profile.instance-profile.name
+
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      instance_interruption_behavior = "stop"
+      spot_instance_type             = "persistent"
+    }
+  }
+
   tags = {
     Name = var.name
   }
 }
 
-instance_market_options {
-  market_type = "spot"
-  spot_options {
-    instance_interruption_behavior = "stop"
-    spot_instance_type             = "persistent"
-  }
-}
 
 resource "aws_security_group" "tool-sg" {
   name        = "${var.name}-sg"
