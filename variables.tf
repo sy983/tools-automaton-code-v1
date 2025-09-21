332453828042bc53aeec2dc536a290e3b93d27f4
suhas yewale <suhasy983@gmail.com>
2025-09-21 23:45:40 +0530

    minikube destroy update

--- variables.tf
+++ variables.tf
@@ -16,14 +16,14 @@ variable "tools" {
       }
       volume_size = 20
     }
-#    minikube = {
-#     instance_type = "t3.medium"
-#      policy_name   = []
-#      ports = {
-#        kube = 8443
-#      }
-#      volume_size = 30
-#       }
+    minikube = {
+     instance_type = "t3.medium"
+      policy_name   = []
+      ports = {
+        kube = 8443
+      }
+      volume_size = 30
+       }
 
   }
 }
